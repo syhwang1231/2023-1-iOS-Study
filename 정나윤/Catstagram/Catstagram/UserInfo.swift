0e2f50bdac53bdf6d09bc60431d332bdd6a9dde9
nyjeong03 <nyjeong03@naver.com>
2023-05-10 01:18:23 +0900

    Feat : 1~4주차 실습

---
+++ 정나윤/Catstagram/Catstagram/UserInfo.swift
@@ -0,0 +1,15 @@
+//
+//  UserInfo.swift
+//  Catstagram
+//
+//  Created by 정나윤 on 2023/04/02.
+//
+
+import UIKit
+
+struct UserInfo{
+    let email: String
+    let name: String
+    let nickname: String
+    let password: String
+}
