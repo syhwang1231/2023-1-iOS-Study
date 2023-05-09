09ef6d27c33822858f044980b49c9e7a4bd79f26
nyjeong03 <nyjeong03@naver.com>
2023-05-10 01:08:35 +0900

    Feat : 4주차 실습

---
+++ 정나윤/4주차/Catstagram/Color+Extention.swift
@@ -0,0 +1,23 @@
+//
+//  Color+Extention.swift
+//  Catstagram
+//
+//  Created by 정나윤 on 2023/04/01.
+//
+
+import UIKit
+
+extension UIColor {
+    static let facebookColor = UIColor(
+        red: 0.255,
+        green: 0.576,
+        blue: 0.947,
+        alpha: 1.0)
+    
+    static let disabledButtonColor = UIColor(
+        red: 0.776,
+        green: 0.872,
+        blue: 0.988,
+        alpha: 1.0)
+                        
+}
